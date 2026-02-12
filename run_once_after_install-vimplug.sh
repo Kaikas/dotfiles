58ca2ac4b6dafca56b7dedc86f9ccb073ea9df04
kaikas <max.riechelmann@eltako.de>
2026-02-12 23:26:51 +0100

    PlugInstall

--- run_once_after_install-vimplug.sh
+++ run_once_after_install-vimplug.sh
@@ -5,8 +5,13 @@ PLUG_PATH="$HOME/.vim/autoload/plug.vim"
 
 if [ ! -f "$PLUG_PATH" ]; then
     echo "Installing vim-plug..."
+
     curl -fLo "$PLUG_PATH" --create-dirs \
         https://raw.githubusercontent.com/junegunn/vim-plug/master/plug.vim
+
+    echo "Running PlugInstall..."
+    vim +PlugInstall +qall
+
 else
     echo "vim-plug already installed."
 fi
