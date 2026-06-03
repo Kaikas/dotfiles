d0ed839f9023680be7a7bdfe681274f78d550653
kaikas <max.riechelmann@googlemail.com>
2026-06-03 09:46:25 +0200

    lockscreen

--- dot_config/i3/executable_lock.sh
+++ dot_config/i3/executable_lock.sh
@@ -5,26 +5,42 @@ SRC="$HOME/Wallpaper/lock.png"
 CACHE_DIR="$HOME/.cache/i3lock"
 mkdir -p "$CACHE_DIR"
 
-res=$(xrandr --query | awk '/ connected primary/{for(i=3;i<=NF;i++) if ($i ~ /^[0-9]+x[0-9]+\+/){split($i,a,"+"); print a[1]; exit}}')
-[[ -z "$res" ]] && res=$(xrandr --query | awk '/ connected/{for(i=2;i<=NF;i++) if ($i ~ /^[0-9]+x[0-9]+\+/){split($i,a,"+"); print a[1]; exit}}')
+# Collect geometries of all connected outputs: WxH+X+Y
+geoms=$(xrandr --query | awk '/ connected/{for(i=2;i<=NF;i++) if ($i ~ /^[0-9]+x[0-9]+\+[0-9]+\+[0-9]+/){print $i; break}}')
+[[ -z "$geoms" ]] && { echo "no outputs found" >&2; exit 1; }
 
 mtime=$(stat -c %Y "$SRC")
-cache="$CACHE_DIR/lock-${res}-${mtime}.png"
+key=$(echo "$geoms" | tr '\n' '_' | sed 's/_$//')
+cache="$CACHE_DIR/lock-${key}-${mtime}.png"
 
 if [[ ! -f "$cache" ]]; then
-  python3 - "$SRC" "$cache" "$res" <<'PY'
+  python3 - "$SRC" "$cache" $geoms <<'PY'
 import sys
 from PIL import Image
-src, dst, res = sys.argv[1], sys.argv[2], sys.argv[3]
-w, h = map(int, res.split('x'))
+
+src, dst = sys.argv[1], sys.argv[2]
+geoms = []
+for g in sys.argv[3:]:
+    wh, x, y = g.split('+')
+    w, h = map(int, wh.split('x'))
+    geoms.append((w, h, int(x), int(y)))
+
+canvas_w = max(x + w for w, h, x, y in geoms)
+canvas_h = max(y + h for w, h, x, y in geoms)
+canvas = Image.new('RGB', (canvas_w, canvas_h), (0, 0, 0))
+
 img = Image.open(src).convert('RGB')
 sw, sh = img.size
-scale = max(w / sw, h / sh)
-nw, nh = int(sw * scale), int(sh * scale)
-img = img.resize((nw, nh), Image.LANCZOS)
-left = (nw - w) // 2
-top = (nh - h) // 2
-img.crop((left, top, left + w, top + h)).save(dst, 'PNG')
+
+for w, h, x, y in geoms:
+    scale = max(w / sw, h / sh)
+    nw, nh = int(sw * scale), int(sh * scale)
+    tile = img.resize((nw, nh), Image.LANCZOS)
+    left = (nw - w) // 2
+    top = (nh - h) // 2
+    canvas.paste(tile.crop((left, top, left + w, top + h)), (x, y))
+
+canvas.save(dst, 'PNG')
 PY
 fi
 
